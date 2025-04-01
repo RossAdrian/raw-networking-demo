a15c2e1c0a905cf44fc0b69efbe32cf6eaaf6502
Adrian Roß <adrian.ross@ross-agentur.de>
2025-04-01 20:16:32 +0200

    Finish link layer

--- demo-ipv4/src/link.rs
+++ demo-ipv4/src/link.rs
@@ -1,5 +1,7 @@
 // Link layer (ethernet)
 
+use std::net::SocketAddr;
+
 /// Create the ARP packet to find gateway MAC address.
 /// 
 /// mac: Our MAC address
@@ -104,4 +106,49 @@ pub fn unwrap_arp(frame: &[u8], mac: [u8; 6], curr_ip: [u8; 4], lookup_ip: [u8; 
 
     // All okay, so return requested MAC
     Some(outp)
+}
+
+/// Create Ethernet packet over IP
+pub fn create_ethernet_packet(src_mac: &[u8; 6], dest_mac: &[u8; 6], src: SocketAddr, dest: SocketAddr) -> Vec<u8> {
+    // Create ethernet frame
+    let mut frame = Vec::new();
+    
+    // push dest/src MAC
+    frame.extend_from_slice(dest_mac);
+    frame.extend_from_slice(src_mac);
+
+    // push Ethertype 0x0800 (IPv4)
+    frame.push(0x08);
+    frame.push(0x00);
+
+    // Create payload of IPv4 packet
+    frame.extend(crate::ip::create_ip_packet(src, dest));
+
+    // return ethernet frame
+    frame
+}
+
+/// Unwrap Ethernet packet to return Timestamp.
+/// 
+/// Traverses up all layers to the application layer, returning the timestamp.
+/// 
+/// Returns None if this packet was not addressed to us, else return the timestamp.
+pub fn unwrap_ethernet_packet(frame: &[u8], our_mac: &[u8; 6], gateway_mac: &[u8; 6], src: SocketAddr, dest: SocketAddr) -> Option<String> {
+    // check we are destination
+    if &frame[0..6] != our_mac {
+        return None;
+    }
+
+    // check source
+    if &frame[6..12] != gateway_mac {
+        return None;
+    }
+
+    // Check ethertype
+    if frame[12..14] != [0x08, 0x00] {
+        return None;
+    }
+
+    // Okay, delegate to IP layer
+    crate::ip::unpack(&frame[14..], src, dest)
 }
